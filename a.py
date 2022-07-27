9d02aad388433c84b5521d1c4dd2b469e55d524d
Martinson1252 <martinson1727@interia.pl>
2022-07-27 15:58:00 +0200

    .
    .

--- a.py
+++
@@ -1,5 +0,0 @@
-class DE:
-    def __init__(self,value) -> None:
-        self.s = value
-    def console(self):
-        print(self.s)
--- b.py
+++
@@ -1,8 +0,0 @@
-from matplotlib.pyplot import cla
-from a import DE
-class F(DE):
-    def __init__(self):
-        DE.console("SIEMA")
-
-d = DE("SA")
-d.console()
