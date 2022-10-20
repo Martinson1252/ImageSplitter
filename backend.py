3ebe5f114d78aa4c3835ebe6b57c2ed2b674f8a1
Martinson1252 <martinson1202@gmail.com>
2022-10-20 23:00:18 +0200

    new

---
+++ backend.py
@@ -0,0 +1,139 @@
+from PyQt5.QtWidgets import (QApplication, QLabel, QLineEdit,QVBoxLayout, QWidget,QMainWindow,QPushButton,QProgressBar,QMessageBox,QDialog)
+from threading import Thread
+import cv2,os
+
+class BackendClass:
+    def __init__(self,ui) -> None:
+        super().__init__()
+        print(ui.ratioHint.text())
+        if not os.path.isdir("source"):
+            os.mkdir("source")
+        if not os.path.isdir("cropped"):
+            os.mkdir("cropped")
+        self.all_filesnumber = 0
+        self.cropped = 0
+        
+    
+    def start(self,ui):
+        #self.lock = Lock()
+        
+        
+        try:
+            self.crop_ratio = float(ui.insertField.text())
+        except:
+            self.ShowErrorBoxNonNUMERIC(ui)
+            return
+        
+        if float(ui.insertField.text()) > 1 or float(ui.insertField.text()) < 0:
+            self.ShowErrorBoxNotFractional(ui)
+            return
+        img_list = os.listdir("source")
+
+        
+        for i in img_list:
+            self.all_filesnumber+=1
+        print(self.all_filesnumber)
+        #print(App.crop_ratio)
+
+        if img_list.count == 0: return
+        if ui.startButton.text() == "Start":
+            ui.startButton.setText("Stop")
+        elif ui.startButton.text() == "Stop":
+            ui.startButton.setText("Start")
+            self.cropped = self.all_filesnumber
+
+        divisior = 10
+        parts = self.all_filesnumber/divisior
+        partsL = [  [] for a in range(divisior)  ]
+        im_num = 0
+        part = 0
+        for i in img_list:
+            
+            if im_num >= parts:
+                part+=1
+                im_num = 0
+            partsL[part].append(i)
+            im_num += 1
+
+        
+        
+        
+        r = 0
+        for i in range(divisior):
+            print(r)
+            t = Thread(target = lambda: self.Split(partsL[r]))
+            t.start()
+            #t.join()
+            r+=1
+            
+        #ImageSplitter.C(self)
+        ui_ = Thread(target= lambda: self.ChangeBar(ui))
+        # #t2 = Thread(target=lambda: ImageSplitter.Split(self))
+        ui_.start()
+
+    def ShowErrorBoxNonNUMERIC(self,ui):
+        dlg = QMessageBox(ui)
+        dlg.setWindowTitle("Error. Wrong value!")
+        dlg.setStandardButtons(QMessageBox.Ok)
+        dlg.setText("Value in crop input field is wrong! Should be numerical and fractional")
+        dlg.setIcon(QMessageBox.Critical)
+        dlg.exec()
+    def ShowErrorBoxNotFractional(self,ui):
+        dlg = QMessageBox(ui)
+        dlg.setWindowTitle("Error. Wrong value!")
+        dlg.setStandardButtons(QMessageBox.Ok)
+        dlg.setText("Value in crop input field is wrong! Should be a fraction; more than 0, and less than 1")
+        dlg.setIcon(QMessageBox.Critical)
+        dlg.exec()
+    
+    def Split(self,img_list):
+
+        #img_list = os.listdir("source")
+        
+        print(img_list)
+        
+        for f in img_list:
+
+            #self.lock.acquire()
+            #cropped_images = self.cropped
+            if self.cropped >= self.all_filesnumber: return
+            image = cv2.imread(os.path.join("source",f))
+            height,width,channels = image.shape
+            crop_imageR = image 
+            crop_imageL = image
+            name = f
+            name = name.removesuffix(".png")
+            name = name + "a"+".png"
+            width_begin=0
+            height_begin=0
+            h=height
+            w=int(width*self.crop_ratio)
+            crop_imageL = crop_imageL[height_begin:h, width_begin:w]
+            cv2.imwrite(os.path.join("cropped",name),crop_imageL)
+
+            name = f
+            name = name.removesuffix(".png")
+            name = name + "b"+".png"
+            width_begin=int(width*self.crop_ratio)
+            height_begin=0
+            h=height
+            w=width
+            crop_imageR = crop_imageR[height_begin:h, width_begin:w]
+            cv2.imwrite(os.path.join("cropped",name),crop_imageR)
+            self.cropped +=1
+            #cropped_images +=1
+            #sleep(0.0000001)
+            #self.cropped = cropped_images
+            #self.lock.release()
+            #print("image "+f+" ready")
+            
+    def ChangeBar(self,ui):
+        while self.cropped < self.all_filesnumber:
+            ui.progressBar.setValue( round( self.cropped/self.all_filesnumber*100 ) )
+            #self.ids.proc.text = str(round(self.cropped/self.all_filesnumber*100))+"%"
+        ui.progressBar.setValue(0)
+        #self.ids.proc.text = ""
+        ui.startButton.setText("Start")
+        self.all_filesnumber = 0
+        self.cropped = 0
+        self.progressBar.setVisible(False)
---
+++ splitter.py
@@ -0,0 +1,71 @@
+from backend import BackendClass 
+import sys
+from tokenize import Double
+from turtle import down
+from PyQt5.QtWidgets import (QApplication, QLabel, QLineEdit,QVBoxLayout, QWidget,QMainWindow,QPushButton,QProgressBar)
+from PyQt5 import QtCore,QtGui
+from PyQt5.QtGui import QCursor,QDoubleValidator,QValidator
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.window().resize(400,400)
+        self.scaleValue = 0.0
+        self.InitGUI()
+        self.backend = BackendClass(self)
+        
+    def InitGUI(self):
+        #text for info
+        self.infoLabel = QLabel(self)
+        self.infoLabel.setGeometry(QtCore.QRect(self.GetHalf(340),15,340,170))
+        self.infoLabel.setText("This program splits images vertically into 2 parts.\nThe place where the single image is split,\nis defined by user's ratio input.\nProgram creates 2 folders: source and cropped.\nIn source folder user has to manually move all\nimages, wheras cropped folder is use to store\ncropped images. Ratio input value is a fraction\n(eg. 0.33) ")
+        
+        #hint for ration input
+        self.ratioHint = QLabel(self)
+        self.ratioHint.setGeometry(QtCore.QRect(self.GetHalf(72),180,72,20))
+        self.ratioHint.setText("Ratio input:")
+        
+        #input field for scale (ratio input)
+        self.insertField = QLineEdit(self)
+        #validation_rule = QDoubleValidator(0.001,0.9999,12)
+        #validation_rule.validate(str(self.scaleValue),12)
+        #self.insertField.setValidator(validation_rule)
+        self.insertField.setGeometry(QtCore.QRect(self.GetHalf(80),200,80,30))
+        self.insertField.textEdited[str].connect(lambda: self.InputChange(self.insertField.text()))
+        
+        #button to start process
+        self.startButton = QPushButton(self)
+        self.startButton.setGeometry(QtCore.QRect(self.GetHalf(100),250,100,30))
+        self.startButton.setText("Start")
+        self.startButton.clicked.connect(lambda: self.backend.start(self))
+        self.startButton.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.startButton.setFont(font)
+        
+        #progress bar
+        self.progressBar = QProgressBar(self)
+        self.progressBar.setGeometry(QtCore.QRect(self.GetHalf(160),300,160,20))
+        self.progressBar.setValue(0)
+        self.progressBar.setVisible(False)
+    
+    def InputChange(self,val):
+        print(val)
+    #Returns correct middle of the screen value (width), for any widget
+    def GetHalf(self,width):
+        return int(self.window().width()/2 - width/2)
+    
+    
+if __name__=="__main__":
+    style = ""
+    with open("stylesheet.style","r") as file:
+        style = (file.read())
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.setWindowTitle("ImageSplitter")
+    
+
+    window.setStyleSheet(style)
+    window.show()
+
+    app.exec()
